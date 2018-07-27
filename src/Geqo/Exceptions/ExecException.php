f9b3c8a691c3700de70eaf6d25f8d7c94bac86bc
Alex <work@geqo.ru>
2018-07-27 15:29:38 +0300

    License fix

--- src/Geqo/Exceptions/ExecException.php
+++ src/Geqo/Exceptions/ExecException.php
@@ -3,7 +3,7 @@
  *
  * Copyright © 2018 Alex White geqo.ru
  * Author: Alex White
- * All rights reserverd
+ * All rights reserved
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
